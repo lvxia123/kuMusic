41007ee2fcfb1e2b55c5d662c7b1b84199d6a3bd
huyingqing <1328437452@qq.com>
2020-09-17 09:49:34 +0800

    添加清清的分支

---
+++ pages/Songs/Songs.vue
@@ -0,0 +1,31 @@
+<template>
+</template>
+
+<script>
+	export default {
+	
+	  data() {
+	    return {
+	     // 声明变量保存推荐数组
+			 songs:[]
+	    }
+	  },
+	  onLoad(opthion,pages) {
+			var opthions=parseInt(opthion)
+			uni.request({
+				url:"http://m.kugou.com/rank/info/?rankid=opthions&page=pages&json=true",
+				success:(res)=>{
+					console.log(res);
+					
+				}
+			})
+	  },
+	  methods: {
+
+				
+	  },
+	}
+</script>
+
+<style>
+</style>
